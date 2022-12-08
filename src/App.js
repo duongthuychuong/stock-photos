2f61df012fb2435370c727459eeaf7edca6230b3
chuongduong <duongthuychuong@gmail.com>
2022-12-08 16:37:29 +0700

    search feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
@@ -8,18 +8,35 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const input = useRef("");
   const handleSubmit = (e) => {
     e.preventDefault();
+    setQuery(input.current.value);
   };
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}${clientID}`;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setPhotos(data);
+      console.log(data);
+      setPhotos((prev) => {
+        if (query) {
+          return [...prev, ...data.results];
+        } else {
+          return [...prev, ...data];
+        }
+      });
     } catch (error) {
       console.log(error);
     } finally {
@@ -28,18 +45,15 @@ function App() {
   };
   useEffect(() => {
     fetchImages();
-  }, []);
+  }, [page, query]);
   useEffect(() => {
     const event = window.addEventListener("scroll", () => {
       if (
         !loading &&
         window.innerHeight + window.scrollY >= document.body.scrollHeight - 15
       ) {
-        console.log("worked");
+        setPage((prev) => prev + 1);
       }
-      console.log(`innerHeight ${window.innerHeight}`);
-      console.log(`scrollY ${window.scrollY}`);
-      console.log(`body height ${document.body.scrollHeight}`);
     });
     return () => {
       window.removeEventListener(event);
@@ -49,7 +63,12 @@ function App() {
     <main>
       <section className="search">
         <form className="search-form">
-          <input type="text" placeholder="Search" className="form-input" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="form-input"
+            ref={input}
+          />
           <button type="submit" className="submit-btn" onClick={handleSubmit}>
             <FaSearch />
           </button>
